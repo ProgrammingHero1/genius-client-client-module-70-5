ab7812d1c240b74fd009dfb0d4471eda9e82a644
Programming Hero <programming.hero1@gmail.com>
2022-11-09 19:39:48 -0600

    implemented search

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useRef } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import ServiceCard from './ServiceCard';
@@ -6,11 +7,17 @@ import ServiceCard from './ServiceCard';
 const Services = () => {
     const [services, setServices] = useState([]);
     const [isAsc, setIsAsc] = useState(true);
+    const [search, setSearch] = useState('');
+    const searchRef = useRef();
     useEffect( () =>{
-        fetch(`http://localhost:5000/services?order=${ isAsc ? 'asc' : 'desc'}`)
+        fetch(`http://localhost:5000/services?search=${search}&order=${ isAsc ? 'asc' : 'desc'}`)
         .then(res =>res.json())
         .then(data => setServices(data))
-    }, [isAsc]);
+    }, [isAsc, search]);
+
+    const handleSearch = () =>{
+        setSearch(searchRef.current.value);
+    }
 
     return (
         <div>
@@ -18,6 +25,8 @@ const Services = () => {
                 <p className="text-2xl font-bold text-orange-600">Services</p>
                 <h2 className="text-5xl font-semibold">Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                <input className='input input-sm' ref={searchRef} type="text" /> 
+                <button onClick={handleSearch}>Search</button>
                 <button className='btn btn-ghost' onClick={() => setIsAsc(!isAsc)}>{isAsc ? 'desc' : 'asc'}</button>
             </div>
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
